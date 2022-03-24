369794859f86b09c42748dee4810ac236ef144b2
Rafael <r-enc@hotmail.com>
2022-03-24 02:30:21 +0000

    Add:New Task

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - calculate binary tree's height
+ * @tree: node binary tree's root
+ * Return: height binary tree (size_t)
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL || (!tree->left && !tree->right))
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+		return (left + 1);
+	else
+		return (right + 1);
+}
